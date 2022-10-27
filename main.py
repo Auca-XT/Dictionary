1aec6ad17b32a828649b909e90bcf60c3a34bcf5
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-10-27 12:59:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+names = {1: "Henk", 2: "Kees", 3: "Jan"}
+#add name "Willem¨ to names
+names [4] = "Willem"
+#print dictonary
+print (names)
+#print value 4 in names
+print(names[4])
+#print value 1 in names
+print(names[1])
